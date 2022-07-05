ec4b38407d527cabccb42e8febd5a491063a8622
Suyog Adhikari <suyogadhiakri@gmail.com>
2022-07-05 18:31:39 +0545

    Initial

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 import { Link, useNavigate} from 'react-router-dom'
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+import { setDoc, doc, serverTimestamp } from 'firebase/firestore'
+import { db } from '../firebase.config'
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
@@ -21,6 +24,33 @@ function SignUp() {
     }))
   }
 
+  const onSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+      const auth = getAuth()
+
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+
+      const user = userCredential.user
+
+      updateProfile(auth.currentUser, {
+        displayName: name
+      })
+
+      const formDataCopy = {...formData}
+      delete formDataCopy.password
+      formDataCopy.timestamp = serverTimestamp()
+
+      await setDoc(doc(db, 'users', user.uid), formDataCopy)
+
+      navigate('/')
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
     return (
       <>
         <div className="pageContainer">
@@ -30,8 +60,7 @@ function SignUp() {
             </p>
           </header>
 
-          <main>
-            <form>
+            <form onSubmit={onSubmit}>
             <input
                 type='text'
                 className='nameInput'
@@ -87,7 +116,6 @@ function SignUp() {
             <Link to='/sign-in' className='registerLink'>
               Sign In Instead
             </Link>
-          </main>
         </div>
       </>
     )
